4490b1492017e1e9df220b8ebe971032adaf0243
sunyoung0 <sunyp0713@naver.com>
2025-10-14 17:59:37 +0900

    feat :20251014 react

---
+++ myapp/src/Drink.jsx
@@ -0,0 +1,5 @@
+function Drink(props) {
+  return <h1>Would you like some {props.drink}?</h1>
+}
+
+export default Drink;
---
+++ myapp/src/Hello.jsx
@@ -0,0 +1,7 @@
+// 파일명과 컴포넌트 명은 같아야함.
+function Hello() {
+
+  return <h1>Hello 김일</h1>
+}
+
+export default Hello;
