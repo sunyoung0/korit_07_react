9553ca0225253b5aa9a25af4df3af8994fcf3d6e
sunyoung0 <sunyp0713@naver.com>
2025-10-15 17:46:26 +0900

    feat : 20251015 contextAPI / handler

---
+++ mycontextapi/src/MyForm.jsx
@@ -0,0 +1,19 @@
+function MyForm() {
+  // Form이 제출될 때 호출될 수 있도록 작성
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // md 파일에서 작성한 기본 동작 방지 메서드
+    // 그러면 onSubmit에 딸려있는 default는 양식 제출. -> DB나 백엔드로
+    alert("제출 시에 나오는 경고창입니다.");
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="submit" value="제출"></input>
+      </form>
+    </>
+  );
+}
+
+export default MyForm;
